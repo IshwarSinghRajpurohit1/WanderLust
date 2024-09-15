abe4c98c15343159cac4cbb7a69e6ef27babb4c4
IshwarSinghRajpurohit1 <ishwarsinghrajpurohit21@gmail.com>
2024-09-15 13:37:44 +0530

    show reviews

--- schema.js
+++ schema.js
@@ -9,5 +9,12 @@ module.exports.listingSchema= Joi.object({
         price:Joi.string().required().min(0),
         image:Joi.string().allow("",null)
 
-    }).required()
-})
+    }).required(),
+});
+
+module.exports.reviewSchema = Joi.object({
+   review: Joi.object({
+    rating: Joi.number().required().min(1).max(5),
+    comment: Joi.string().required(),
+   }).required(),
+});
