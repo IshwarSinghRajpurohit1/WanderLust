7dd36a78527818f15bda38154530ad3687b8e979
IshwarSinghRajpurohit1 <ishwarsinghrajpurohit21@gmail.com>
2024-09-23 10:57:49 +0530

    Apply authentication and authorisation

--- init/index.js
+++ init/index.js
@@ -18,6 +18,7 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data=initData.data.map((obj)=>({...obj, owner: "66efce677fe270ca79c9331e" }));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
